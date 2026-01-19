588f0bf21b465b2771f4df564ef96b10f8a692ed
salasaa <akhirudinsalasa.gc@gmail.com>
2026-01-20 01:00:30 +0700

    feat: update DashboardForm layout and display user information

--- app/components/form/dashboard-form.tsx
+++ app/components/form/dashboard-form.tsx
@@ -1,26 +1,14 @@
-import Cookies from "js-cookie";
+import { Card } from "~/components/ui/card";
 
-import { cn } from "~/lib/utils";
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
-import {
-  Field,
-  FieldGroup,
-  FieldLabel,
-  FieldDescription,
-  FieldSeparator,
-} from "~/components/ui/field";
 import type { User } from "~/modules/user/type";
-import { formatEventDateOnly } from "~/lib/format";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { MyEventsLists } from "./my-events-lists";
 
 interface DashboardFormProps {
   meResponse: User;
 }
 
-export function DashboardForm({
-  meResponse,
-}: React.ComponentProps<"div"> & DashboardFormProps) {
+export function DashboardForm({ meResponse }: DashboardFormProps) {
   const [myEvents, setMyEvents] = useState<{ total: number; data: any[] }>({
     total: 0,
     data: [],
@@ -28,15 +16,21 @@ export function DashboardForm({
 
   return (
     <div>
-      <Card className="overflow-hidden">
-        <section className="p-4 md:p-6 grid grid-cols-1 sm:grid-cols-2">
+      <section className="grid grid-cols-2 sm:grid-cols-2 gap-4 mb-4">
+        <Card className="p-4 md:p-6">
           <div className="flex flex-col ">
             <h1 className="text-2xl font-bold">{meResponse.fullName}</h1>
             <p className="text-muted-foreground">{meResponse.username}</p>
             <p className="text-gray-300">{meResponse.email}</p>
           </div>
-        </section>
-      </Card>
+        </Card>
+        <Card className="p-4 md:p-6 bg-gray-800">
+          <div className="flex flex-col ">
+            <h1 className="text-2xl text-white font-bold">Events Journey</h1>
+            <p className="text-muted-foreground">You have joined :</p>
+          </div>
+        </Card>
+      </section>
 
       <MyEventsLists meResponse={meResponse} />
     </div>
--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -64,7 +64,7 @@ export default function Dashboard({ loaderData }: Route.ComponentProps) {
   }, []);
 
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+    <div className="bg-muted flex min-h-svh flex-col items-center p-6 md:p-8">
       <div className="w-full max-w-sm md:max-w-4xl">
         <DashboardForm meResponse={meResponse} />
       </div>
